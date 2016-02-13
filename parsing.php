4522375ab6546c08abee7465c844c05bd0b27bfa
iltempe <pratosmart@gmail.com>
2016-02-13 01:47:39 +0100

    refactoring parser

--- parsing.php
+++ parsing.php
@@ -23,28 +23,54 @@ function parsing_albo_vallata($url) {
 	$xpath = new DOMXPath($dom);
  
 //XPath Query
-//$my_xpath_query = "//body/div[1]/div";
+	
 	$my_xpath_query = "//p";
-  $data = array();
+	$res1 = $xpath->query($my_xpath_query);
+   	
+   	$my_xpath_query2 = "//a/@href";
+	$res2 = $xpath->query($my_xpath_query2);
 
-$res = $xpath->query($my_xpath_query);
-
-foreach($res as $span)
+foreach($res1 as $span1)
    {
-   	 //echo get_string_between($span->nodeValue, "Data pubblicazione:", "Data scadenza:");
-     $stringa = $span->nodeValue;
-     $data[] = get_string_between($span->nodeValue, "Data pubblicazione:", "Data scadenza:");
-     print_r($span->nodeValue);
-     print_r("---------------");
-    }	
+     $string = $span1->nodeValue;
+ 	 $string = str_do_better($string);
+    }
+foreach($res2 as $span2)
+   {
+     $links[] = "http://194.243.23.67:8080/albopretorio/" .$span2->nodeValue;
+    }    
+	
+$string_arr = explode(":", $string);
+unset($string_arr[0]);
 
-   
-   	//$my_xpath_query = "//a/@href";
-	//$res = $xpath->query($my_xpath_query);
+$string_arr_items=array_chunk($string_arr, 5);
 
-	//foreach($res as $span)
-   	//{
-	// echo $span->nodeValue;     
-   	//}	
+for($i=0, $size = count($string_arr_items); $i<=$size; $i++) {
+	$string_arr_items[$i]=format_item($string_arr_items[$i]);
+	echo $i;
+}
+print_r($string_arr_items);
+
+//LINKS
+print_r($links);
+
+}
+
+function str_do_better($string)
+{
+    $string = preg_replace('/^[ \t]*[\r\n]+/m', '', $string);
+	$string = str_replace("\n", "", $string);
+	$string = str_replace("\r", "", $string);
+	return $string;
+}
+
+function format_item($array)
+{
+	$item[0]=str_replace("    Tipo", "",$array[0]);
+	$item[1]=str_replace("    Data pubblicazione", "",$array[1]);
+	$item[2]=str_replace("    Data scadenza", "",$array[2]);
+	$item[3]=str_replace("Oggetto", "",$array[3]);
+	$item[4]=str_replace("Documento Numero", "",$array[4]);
+	return $item;
 }
 
